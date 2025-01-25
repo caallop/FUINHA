8a3cdc4851225c128bb24ecb2bb6abe97d8c6c18
caallop <69176501+caallop@users.noreply.github.com>
2025-01-24 21:55:56 -0300

    Add files via upload

---
+++ dante pedidos/scipt.js
@@ -0,0 +1,9 @@
+let pica = false
+function duro(){
+    pica = true
+    document.getElementById('pipi').src = "img/duro.jpg"
+}
+function mole(){
+        pica = false
+        document.getElementById('pipi').src = "img/mole.jpg"
+}
